e4f7c131ae1ed137c0e4070b7e09c151fc1d1798
Holger Grosse-Plankermann <holgergp@gmail.com>
2025-03-13 19:36:29 +0100

    feat(prediction): calculation does not subtract anymore

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 export function predictPrice(basePrice: number): number {
-    const adjustment = (Math.random() * 0.1) * basePrice;
-    const addOrSubtract = Math.random() < 0.5 ? -1 : 1;
-    return basePrice + (adjustment * addOrSubtract);
+    const adjustment = (Math.random() * 0.2) * basePrice;
+    return basePrice + adjustment;
 }
